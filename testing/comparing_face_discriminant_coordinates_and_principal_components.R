7706e8d7fbf1153b68d307ff074ce191162e6ed8
mcomas <mar.comas@udg.edu>
2019-07-15 07:25:12 +0200

    Discriminant examples

--- testing/comparing_face_discriminant_coordinates_and_principal_components.R
+++ testing/comparing_face_discriminant_coordinates_and_principal_components.R
@@ -1,8 +1,11 @@
-X = iris[,1:4]
-K = ncol(X)
-gr = iris$Species
-
 library(fpc)
+set.seed(1)
+face = rFace(600,dMoNo=2,dNoEy=0)
+X = as.data.frame(face)
+K = ncol(X)
+gr = attr(face, 'grouping')
+
+
 dcf <- discrcoord(X,gr)
 plot(dcf$proj,col=gr)
 
@@ -27,8 +30,9 @@ POST = sapply(1:nG, function(i){
 
 library(coda.base)
 PC.coord = coordinates(POST, 'pc')
-plot(-PC.coord[,1], -PC.coord[,2], col=gr)
-apply(PC.coord, 2, var)
+plot(PC.coord[,1], PC.coord[,2], col=gr)
+v = apply(PC.coord, 2, var)
+cumsum(v) / sum(v)
 attr(PC.coord, 'basis')
 
 PB = pb_basis(POST, method = 'exact')
@@ -45,4 +49,27 @@ sign(PB)
 PB.coord = coordinates(POST, basis = PB)
 plot(PB.coord[,1:2], col=gr)
 legend('bottomright', legend =levels(gr), pch=1, col=1:3)
-apply(PB.coord, 2, var)
+
+v = apply(PB.coord, 2, var)
+vtotal = sum(v)
+cumsum(v)/sum(v)
+
+
+# pc1       pc2       pc3       pc4       pc5
+# 0.7396288 0.9740265 0.9979777 0.9991101 1.0000000
+
+# PRINCIPAL BALANCES
+# x1        x2        x3        x4        x5
+# 0.6711526 0.8300339 0.9834595 0.9988723 1.0000000
+
+sign(PB)[,1:2]
+
+table(gr)
+M = sbp_basis(V2+V6~V1+V3+V4,
+              V2~V5, data = X)
+PB.coord = coordinates(POST, M)
+plot(PB.coord, col = gr)
+v = apply(PB.coord, 2, var)
+cumsum(v)/vtotal
+
+
